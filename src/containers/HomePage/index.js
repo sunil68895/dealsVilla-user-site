1f120b8e485c89499a0530798bb26cdb5eedbb02
sunil sharma <sunil68895@gmail.com>
2021-05-22 11:37:21 +0530

    first commit

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../../components/Header'
+import Layout from '../../components/Layout'
+import MenuHeader from '../../components/MenuHeader'
+
+/**
+* @author
+* @function HomePage
+**/
+
+const HomePage = (props) => {
+  return(
+    <Layout>
+      DealsVilla
+    </Layout>
+   )
+
+ }
+
+export default HomePage
