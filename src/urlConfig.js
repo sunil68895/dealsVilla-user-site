c619ca93f083a1e426afa7bba93348c4f532b46a
sunil sharma <sunil68895@gmail.com>
2021-05-23 12:03:22 +0530

    update package.json

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
 const baseUrl = "https://dealsvilla-backend.herokuapp.com";
-// const baseUrl = "http://localhost:2000";
+//  const baseUrl = "http://localhost:2000";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
   return `${baseUrl}/public/${fileName}`;
