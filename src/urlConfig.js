a6502743930b78e9928f2cecec0b0d6aad5f97bb
sunil sharma <sunil68895@gmail.com>
2021-05-23 11:30:05 +0530

    update Urlconfig

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = "https://flipkart-rest-server.herokuapp.com";
+const baseUrl = "https://dealsvilla-backend.herokuapp.com";
 // const baseUrl = "http://localhost:2000";
 export const api = `${baseUrl}/api`;
 export const generatePublicUrl = (fileName) => {
