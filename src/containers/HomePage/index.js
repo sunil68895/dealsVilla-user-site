c0625d574587edbf93c018fa09b6f633274b63b2
sunil68895 <72141073+sunil68895@users.noreply.github.com>
2021-07-20 18:52:30 +0530

    Update index.js

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -2,6 +2,11 @@ import React from 'react'
 import Header from '../../components/Header'
 import Layout from '../../components/Layout'
 import MenuHeader from '../../components/MenuHeader'
+import  { Carousel }  from 'react-responsive-carousel'
+import c11 from '../../images/c11.jpg'
+import c21 from '../../images/c21.jpg'
+import c31 from '../../images/c31.jpg'
+import c41 from '../../images/c41.jpg'
 
 /**
 * @author
@@ -9,9 +14,25 @@ import MenuHeader from '../../components/MenuHeader'
 **/
 
 const HomePage = (props) => {
+
+  const  items=[
+      {id: 1 , url: c41},
+      {id: 2 , url: c31},
+      {id: 3 , url: c21},
+      {id: 4 , url: c11},
+    ]
   return(
     <Layout>
-      DealsVilla
+
+      <Carousel showThumbs={false} interval={3000} infiniteLoop={true} autoPlay={true} centerMode={true}>
+        {items.map(item=><div key={item.id} style={{
+        height:'600px',
+        display:'flex',
+      }}>
+      <img src={item.url} alt="" />
+       </div>)
+      }
+      </Carousel>
     </Layout>
    )
 
